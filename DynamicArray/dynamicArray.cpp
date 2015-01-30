49ef37835ea01f96f075cac434ed66449a8fd938
Raphaël Doré <rdore@neomalibox.ch>
2015-01-30 10:37:33 -0500

    Tous les éléments du tableau sont initialisés à 0

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -3,9 +3,15 @@
 DynamicArray::DynamicArray()
 {
 	this->capacite = 100;
+	this->tabElements = new int[100]();
 }
 
 int DynamicArray::getCapacite()
 {
 	return this->capacite;
+}
+
+int DynamicArray::getElement(int i)
+{
+	return this->tabElements[i];
 }
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -1,20 +1,19 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
-
 class DynamicArray
 {
 
 public:
-
-
 	DynamicArray();
 
 	int getCapacite();
+	int getElement(int i);
 
 
 private:
 	unsigned int capacite;
+	int * tabElements;
 };
 
 
